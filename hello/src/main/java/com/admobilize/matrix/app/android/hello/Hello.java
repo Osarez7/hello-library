85057a45fbef0a9e4041dc8f2d467bcea5d5a6de
Julio Saldaña <julio.saldana@admobilize.com>
2017-07-17 10:21:30 -0500

    hello library

---
+++ hello/src/main/java/com/admobilize/matrix/app/android/hello/Hello.java
@@ -0,0 +1,15 @@
+package com.admobilize.matrix.app.android.hello;
+
+import android.util.Log;
+
+/**
+ * Created by juliosaldana on 7/17/17.
+ */
+
+public class Hello {
+   private static final String TAG = Hello.class.getSimpleName();
+
+    public  static void sayHello(){
+        Log.d(TAG, "Hello world");
+    }
+}
